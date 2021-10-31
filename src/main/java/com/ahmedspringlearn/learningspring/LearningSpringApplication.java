c2230f7360dd7070cb612b99902df2012cf3f4a2
Ahmed Bughra <ahmedbughra@gmail.com>
2021-10-31 17:27:32 +0300

    Chapter 4 lesson4
    https://www.linkedin.com/learning/learning-spring-with-spring-boot-2/put-it-all-together?autoAdvance=true&autoSkip=true&autoplay=true&contextUrn=urn%3Ali%3AlyndaLearningPath%3A5b101b04498e06fb6e2d8785&resume=false&u=105858922

--- src/main/java/com/ahmedspringlearn/learningspring/LearningSpringApplication.java
+++ src/main/java/com/ahmedspringlearn/learningspring/LearningSpringApplication.java
@@ -16,40 +16,53 @@ public class LearningSpringApplication {
 		SpringApplication.run(LearningSpringApplication.class, args);
 	}
 
-	@RestController
-	@RequestMapping("/rooms")
-	public class RoomController {
-		@Autowired
-		private RoomRepository roomRepository;
 
-		@GetMapping
-		public Iterable<Room> getRooms() {
-			return  this.roomRepository.findAll();
-		}
-	}
 
-	@RestController
-	@RequestMapping("/guests")
-	public class GuestController {
-		@Autowired
-		private RoomRepository guestRepository;
 
-		@GetMapping
-		public Iterable<Room> getRooms() {
-			return  this.guestRepository.findAll();
-		}
-	}
 
-	@RestController
-	@RequestMapping("/reservations")
-	public class ReservationController {
-		@Autowired
-		private RoomRepository ReservationRepository;
 
-		@GetMapping
-		public Iterable<Room> getRooms() {
-			return  this.ReservationRepository.findAll();
-		}
-	}
+
+
+
+
+
+	// These anotations have used the last chapter of the tutorial
+	// https://www.linkedin.com/learning/learning-spring-with-spring-boot-2/build-your-first-controller?autoAdvance=true&autoSkip=true&autoplay=true&contextUrn=urn%3Ali%3AlyndaLearningPath%3A5b101b04498e06fb6e2d8785&resume=false&u=105858922
+//
+//	@RestController
+//	@RequestMapping("/rooms")
+//	public class RoomController {
+//		@Autowired
+//		private RoomRepository roomRepository;
+//
+//		@GetMapping
+//		public Iterable<Room> getRooms() {
+//			return  this.roomRepository.findAll();
+//		}
+//	}
+//
+//	@RestController
+//	@RequestMapping("/guests")
+//	public class GuestController {
+//		@Autowired
+//		private RoomRepository guestRepository;
+//
+//		@GetMapping
+//		public Iterable<Room> getRooms() {
+//			return  this.guestRepository.findAll();
+//		}
+//	}
+//
+//	@RestController
+//	@RequestMapping("/reservations")
+//	public class ReservationController {
+//		@Autowired
+//		private RoomRepository ReservationRepository;
+//
+//		@GetMapping
+//		public Iterable<Room> getRooms() {
+//			return  this.ReservationRepository.findAll();
+//		}
+//	}
 
 }
